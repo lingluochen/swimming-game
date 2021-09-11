8f226f170aa9afe1a13be5890166d85f6f4ff859
hongke <hl3407@nyu.edu>
2021-09-10 22:27:52 -0400

    random swimmer speed

--- Assets/Scripts/SwimmerMove.cs
+++ Assets/Scripts/SwimmerMove.cs
@@ -11,7 +11,7 @@ public class SwimmerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(1.0f, 5.0f);
     }
 
     // Update is called once per frame
