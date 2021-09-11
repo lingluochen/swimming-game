315b08ea6737c7d883de05edc52ceb986120bbd0
hongke <hl3407@nyu.edu>
2021-09-10 23:04:17 -0400

    more random speed

--- Assets/Scripts/ScreenWrap.cs
+++ Assets/Scripts/ScreenWrap.cs
@@ -6,11 +6,12 @@ public class ScreenWrap : MonoBehaviour
 {
     public bool TopRow;
     public bool BotRow;
+    public SwimmerMove swimmermove;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        swimmermove = GetComponent<SwimmerMove>();
     }
 
     // Update is called once per frame
@@ -18,11 +19,13 @@ public class ScreenWrap : MonoBehaviour
     {
         if (transform.position.x >= 7 && BotRow) {
             transform.position = new Vector3(-6.5757f, transform.position.y, transform.position.z);
+            swimmermove.speed = Random.Range(1.0f, 5.0f);
         }
 
         else if (transform.position.x <= -6.5757f && TopRow)
         {
             transform.position = new Vector3(7, transform.position.y, transform.position.z);
+            swimmermove.speed = Random.Range(1.0f, 5.0f);
         }
         Debug.Log("moving");
         Debug.Log(transform.position.x);
